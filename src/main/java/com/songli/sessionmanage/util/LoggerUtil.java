0815afee2a556e0e92acdde748455331942b00fe
Songli Li <jifucishi@vip.qq.com>
2014-10-07 00:02:55 +0800

    New project

---
+++ src/main/java/com/songli/sessionmanage/util/LoggerUtil.java
@@ -0,0 +1,17 @@
+package com.songli.sessionmanage.util;
+
+import org.apache.commons.logging.Log;
+
+public class LoggerUtil {
+	private Log logger;
+	
+	public LoggerUtil(Log logger) {
+		this.logger = logger;
+	}
+	
+	public void info(String message, Object... params) {
+		
+	}
+	
+	
+}
